66d8488057f26d7aae7ee3b9b00cd478e9015e54
Tom Higgins <tomhiggins@gmail.com>
2020-11-03 16:08:32 -0600

    Update WIndowsOneLineOneShowPerPodcastSave.ps1

--- WIndowsOneLineOneShowPerPodcastSave.ps1
+++ WIndowsOneLineOneShowPerPodcastSave.ps1
@@ -1,1 +1,1 @@
-wip
+del play.m3u;foreach ($podcast in (Get-Content "podcasts.txt")) {Invoke-WebRequest -Uri $podcast -Outfile 'feed.xml' ;[xml]$Content = Get-Content feed.xml;$fname=($Content.rss.channel.Item.enclosure.url|select-object -first 1); echo $fname| Out-file play.m3u -Append;Invoke-WebRequest -Uri (echo $fname) |Out-File (Split-Path ($fname) -Leaf) -NoClobber }
